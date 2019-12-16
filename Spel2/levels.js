5b613c63544008d4befc1578b10784f0e5c66f2d
Isak05 <51046377+Isak05@users.noreply.github.com>
2019-12-16 12:00:30 +0100

    Add files via upload

---
+++ Spel2/levels.js
@@ -0,0 +1,188 @@
+data = [];
+
+data.push(new level());
+data[0].walls.push(new wall(0.7, 0.9, 0.8, 0.05, 1));
+data[0].walls.push(new wall(1.45, 0.3, 0.05, 0.6, 1));
+data[0].walls.push(new wall(0.7, 0.3, 0.05, 0.6, 1));
+data[0].walls.push(new wall(0.2, 0.3, 0.3, 0.05, 1));
+data[0].walls.push(new wall(0.7, 0.05, 0.8, 0.05, 1));
+data[0].walls.push(new wall(0.9, 0.6, 0.4, 0.02, 1));
+data[0].walls.push(new wall(0.5, 0.7, 0.2, 0.01, 1));
+data[0].walls.push(new wall(0.2, 0.9, 0.6, 0.05, 1));
+data[0].walls.push(new wall(-0.06640625, -0.006510416666666667, 0.45002442398299025, 0.056806793640777266, 1));
+data[0].walls.push(new wall(0.13411458333333334, -0.3684895833333333, 0.6783470657011753, 0.03932291666654527, 1));
+data[0].walls.push(new wall(0.09635416666666667, -0.3645833333333333, 0.052044982367595694, 0.38958333333333295, 1));
+data[0].walls.push(new wall(0.7760416666666666, -0.3606770833333333, 0.052044982367595694, 0.43124999999999963, 1));
+
+data[0].backgrounds.push(new background(-1, -1, 4, 3, 2, true, 0.2));
+
+data[0].crates.push(new crate(0.2, -0.2, 0.1, 0.1, 7));
+
+data[0].buttons.push(new button(0.6, 0.885));
+
+function level() {
+  this.walls = [];
+  this.backgrounds = [];
+  this.crates = [];
+  this.buttons = [];
+}
+
+function button(x, y) {
+  this.pos = {x, y};
+  this.size = {x: 0.1, y: 0.015};
+  this.texture = 6;
+  this.pressed = false;
+}
+
+function crate(x, y, width, height, texture) {
+  this.pos = {x, y};
+  this.vel = {x: 0, y: 0};
+  this.size = {x: width, y: height};
+  this.texture = texture;
+  this.update = function() {
+    this.vel.y += gravity;
+    this.vel.x = 0;
+    
+    // Collision
+    this.next = new crate(this.pos.x, this.pos.y, this.size.x, this.size.y);
+    this.next.vel.x = this.vel.x;
+    this.next.vel.y = this.vel.y;
+    
+    this.player_ = JSON.parse(JSON.stringify(player));
+    if(player.onGround) {
+      this.player_.vel.x /= 1.7;
+    } else {
+      this.player_.vel.x /= 1.15;
+    }
+    this.player_.vel.x *= player.crateSpeed;
+    this.player_.pos.x += this.player_.vel.x;
+    this.player_.pos.y += this.player_.vel.y;
+    
+    if(!cheatMode) {
+      if(player.pos.y + player.size.y > this.pos.y) {
+        if(checkCollision(this.next, this.player_)) {
+          player.onCrate = true;
+          // Going right on left side
+          if(this.player_.acc.x > 0 && 
+            this.player_.pos.x + this.player_.size.x / 2 < 
+            this.pos.x + this.size.x / 2) {
+
+            player.vel.x *= player.crateSpeed;
+            this.vel.x = (player.pos.x + player.vel.x + player.size.x - 1) - this.pos.x;
+          }
+            
+          // Going left on right side
+          if(this.player_.acc.x < 0 && 
+            this.player_.pos.x + this.player_.size.x / 2 > 
+            this.pos.x + this.size.x / 2) {
+
+            player.vel.x *= player.crateSpeed;
+            this.vel.x = (player.pos.x + player.vel.x - this.size.x + 1) - this.pos.x;
+          }
+          
+          if(!player.interacting && player.prevInteracting) {
+            if(this.player_.pos.x + this.player_.size.x / 2 < 
+              this.pos.x + this.size.x / 2) {
+              
+              this.pos.x = player.pos.x + player.size.x;
+            }
+
+            if(this.player_.pos.x + this.player_.size.x / 2 > 
+              this.pos.x + this.size.x / 2) {
+              
+              this.pos.x = player.pos.x - this.size.x;
+            }
+          }
+        }
+        if(player.interacting) {
+          this.player_.size.x += Math.abs(player.vel.x) * 2 + 2;
+          this.player_.pos.x += -(Math.abs(player.vel.x) + 1);
+          if(checkCollision(this.next, this.player_)) {
+            player.onCrate = true;
+            // Going right on right side
+            if(this.player_.acc.x > 0 && 
+              this.player_.pos.x + this.player_.size.x / 2 > 
+              this.pos.x + this.size.x / 2) {
+
+              player.vel.x *= player.crateSpeed;
+              this.vel.x = (player.pos.x + player.vel.x - this.size.x + 1) - this.pos.x;
+            }
+
+            // Going left on left side
+            if(this.player_.acc.x < 0 && 
+              this.player_.pos.x + this.player_.size.x / 2 < 
+              this.pos.x + this.size.x / 2) {
+
+              player.vel.x *= player.crateSpeed;
+              this.vel.x = (player.pos.x + player.vel.x + player.size.x - 1) - this.pos.x;
+              
+            }
+          }
+        }
+      }
+    }
+    for(var i = 0; i < levels[0].walls.length; i++) {
+      var xVel = this.vel.x;
+      var yVel = this.vel.y;
+      
+      this.next.pos.x += this.vel.x;
+      if(checkCollision(this.next, levels[0].walls[i])) {
+        // Get closest side
+        if(Math.abs((levels[0].walls[i].pos.x + levels[0].walls[i].size.x) - this.pos.x) < 
+          Math.abs(levels[0].walls[i].pos.x - (this.pos.x + this.size.x))) {
+          // Left
+          xVel = 0;
+          this.pos.x = levels[0].walls[i].pos.x + levels[0].walls[i].size.x;
+        } else {
+          // Right
+          xVel = 0;
+          this.pos.x = levels[0].walls[i].pos.x - this.size.x;
+        }
+      }
+      
+      this.next.pos.x -= this.vel.x;
+      this.next.pos.y += this.vel.y;
+      if(checkCollision(this.next, levels[0].walls[i])) {
+        // Get closest side
+        if(Math.abs((levels[0].walls[i].pos.y + levels[0].walls[i].size.y) - this.pos.y) < 
+          Math.abs(levels[0].walls[i].pos.y - (this.pos.y + this.size.y))) {
+          // Up
+          yVel = 0;
+          this.pos.y = levels[0].walls[i].pos.y + levels[0].walls[i].size.y;
+        } else {
+          // Down
+          yVel = 0;
+          this.pos.y = levels[0].walls[i].pos.y - this.size.y;
+        }
+      }
+      
+      this.next.pos.y -= this.vel.y;
+      
+      this.vel.x = xVel;
+      this.vel.y = yVel;
+    }
+    
+    for(var i = 0; i < levels[0].buttons.length; i++) {
+      if(checkCollision(levels[0].buttons[i], this)) {
+        levels[0].buttons[i].pressed = true;
+      }
+    }
+    
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+  }
+}
+
+function wall(x, y, width, height, texture) {
+  this.pos = {x, y};
+  this.size = {x: width, y: height};
+  this.texture = texture;
+}
+
+function background(x, y, width, height, texture, repeating, repeatSize) {
+  this.pos = {x, y};
+  this.size = {x: width, y: height};
+  this.texture = texture;
+  this.repeating = repeating;
+  this.repeatSize = repeatSize;
+}
---
+++ Spel2/main.js
@@ -0,0 +1,469 @@
+var c = document.getElementById("c");
+c.width = screen.width;
+c.height = screen.height;
+var ctx = c.getContext("2d");
+ctx.imageSmoothingEnabled = false;
+
+var player = {
+  pos: new vec2(c.height * 0.7, c.height * 0.15),
+  size: {x: c.height * 0.06, y: c.height * 0.1},
+  vel: new vec2(0, 0),
+  acc: new vec2(0, 0),
+  prevAcc: new vec2(0, 0),
+  speed: c.height * 0.01,
+  crateSpeed: 0.666,
+  interacting: false,
+  prevInteracting: false,
+  onGround: false,
+  onWall: false,
+  onCrate: false,
+  wallJumped: false,
+  wallJumpStrength: c.height * 0.02,
+  jumpStrength: c.height * 0.035,
+  anims: [[{texture: 0, time: 5}, {texture: 3, time: 5}],
+          [{texture: 0, time: 0}],
+          [{texture: 4, time: 0}],
+          [{texture: 5, time: 0}], 
+          [{texture: 4, time: 0}]],
+  animTimer: 0,
+  currentAnimFrame: 0,
+  currentAnim: 1,
+  texture: 0,
+  textureFlipped: false,
+  setAnim: function(x) {
+    this.animTimer = 0;
+    this.currentAnimFrame = 0;
+    this.currentAnim = x;
+    this.texture = this.anims[x][this.currentAnimFrame].texture;
+  }
+};
+
+function vec2(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+var fps = 30;
+var cheatMode = false;
+var building = false;
+var gravity = c.height * 0.0025;
+var cameraOffset = new vec2(0, 0);
+var levels = data;
+// The object arrays are in order of rendering
+var objectNames = ["backgrounds", "crates", "walls", "buttons"];
+var objectColliders = ["", "collide(side, object)", "collide(side, object)", "object.pressed = true"];
+for(var i = 0; i < levels.length; i++) {
+  for(var j = 0; j < objectNames.length; j++) {
+    var objectLength = eval("levels[i]." + objectNames[j] + ".length");
+    for(var k = 0; k < objectLength; k++) {
+      eval("levels[i]." + objectNames[j] + "[k].pos.x *= c.height");
+      eval("levels[i]." + objectNames[j] + "[k].pos.y *= c.height");
+      eval("levels[i]." + objectNames[j] + "[k].size.x *= c.height");
+      eval("levels[i]." + objectNames[j] + "[k].size.y *= c.height");
+    }
+  }
+}
+
+var textureFiles = ["boi", "wall", "brick", "boi2", "boi3", "boi4", "crate", "crate2"];
+var textures = [];
+for(var i = 0; i < textureFiles.length; i++) {
+  textures.push(new Image());
+  textures[i].src = "Textures/" + textureFiles[i] + ".png";
+}
+
+var prevTime = 0;
+var time = 0;
+var actualFps = 0;
+
+var loop = setInterval(update, 1000 / fps);
+function update() {
+  time = window.performance.now();
+  
+  // Movement
+  if(player.onGround || cheatMode) {
+    player.vel.x += player.acc.x;
+  } else {
+    player.vel.x += player.acc.x / 4;
+  }
+  player.vel.y += player.acc.y;
+
+  player.onGround = false;
+  player.onWall = false;
+  player.onCrate = false;
+  if(!cheatMode) {
+    player.vel.y += gravity;
+  }
+  
+  // Buttons
+  for(var i = 0; i < levels[0].buttons.length; i++) {
+    levels[0].buttons[i].pressed = false;
+  }
+  
+  // Crates
+  for(var i = 0; i < levels[0].crates.length; i++) {
+    levels[0].crates[i].update();
+  }
+
+  // Collisions
+  for(var i = 0; i < levels.length; i++) {
+    for(var j = 0; j < objectNames.length; j++) {
+      collideObjects(eval("levels[i]." + objectNames[j]), objectColliders[j]);
+    }
+  }
+  
+  // Buttons
+  for(var i = 0; i < levels[0].buttons.length; i++) {
+    if(levels[0].buttons[i].pressed == true && levels[0].buttons[i].size.y != 0.005 * c.height) {
+      levels[0].buttons[i].size.y = 0.005 * c.height;
+      levels[0].buttons[i].pos.y += 0.01 * c.height;
+    }
+    if(levels[0].buttons[i].pressed == false && levels[0].buttons[i].size.y != 0.015 * c.height) {
+      levels[0].buttons[i].size.y = 0.015 * c.height;
+      levels[0].buttons[i].pos.y -= 0.01 * c.height;
+    }
+  }
+  
+  // Animations
+  if(player.acc.x != 0 && (player.currentAnim != 0 || player.acc.x != player.prevAcc.x) && player.onGround) {
+    player.setAnim(0);
+    if(player.acc.x > 0) {
+      player.textureFlipped = false;
+    } else {
+      player.textureFlipped = true;
+    }
+  }
+  
+  if(player.onGround && player.currentAnim != 1 && player.acc.x == 0) {
+    player.setAnim(1);
+    player.textureFlipped = false;
+  }
+  
+  if(!player.onGround && player.currentAnim != 3) {
+    player.setAnim(3);
+    player.textureFlipped = false;
+  }
+  
+  if(player.onWall && player.currentAnim != 2) {
+    player.setAnim(2);
+    if(player.acc.x > 0) {
+      player.textureFlipped = false;
+    } else {
+      player.textureFlipped = true;
+    }
+  }
+  
+  if(player.onCrate && player.currentAnim != 4) {
+    player.setAnim(4);
+    if(player.acc.x > 0) {
+      player.textureFlipped = false;
+    } else {
+      player.textureFlipped = true;
+    }
+  }
+  
+  if(cheatMode && player.currentAnim != 1) {
+    player.setAnim(1);
+    player.textureFlipped = false;
+  }
+  
+  player.animTimer++;
+  if(player.animTimer >= player.anims[player.currentAnim][player.currentAnimFrame].time) {
+    player.animTimer = 0;
+    if(player.currentAnimFrame < player.anims[player.currentAnim].length - 1) {
+      player.currentAnimFrame++;
+    } else {
+      player.currentAnimFrame = 0;
+    }
+    player.texture = player.anims[player.currentAnim][player.currentAnimFrame].texture;
+  }
+  
+  // Friction
+  if(!cheatMode) {
+    if(player.onGround) {
+      player.vel.x /= 1.7;
+    } else {
+      player.vel.x /= 1.15;
+    }
+    if(player.onWall) {
+      player.vel.y /= 1.5;
+    }
+  }
+  
+  // Player and camera movement
+  player.pos.x += player.vel.x;
+  player.pos.y += player.vel.y;
+
+  cameraOffset.x += ((player.pos.x - c.width * 0.5) - cameraOffset.x) * 0.1;
+  cameraOffset.y += ((player.pos.y - c.height * 0.5 + player.size.y) - cameraOffset.y) * 0.1;
+  
+  player.prevAcc.x = player.acc.x;
+  player.prevAcc.y = player.acc.y;
+  
+  player.prevInteracting = player.interacting;
+
+  draw();
+  
+  actualFps = 1000 / (time - prevTime);
+  prevTime = time;
+}
+
+function draw() {
+  // Clear screen
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fillRect(0, 0, c.width, c.height);
+  
+  // Objects
+  for(var i = 0; i < levels.length; i++) {
+    for(var j = 0; j < objectNames.length; j++) {
+      var objects = eval("levels[i]." + objectNames[j]);
+      for(var k = 0; k < objects.length; k++) {
+        if(objects[k].repeating) {
+          var pattern = ctx.createPattern(textures[objects[k].texture], "repeat");
+          ctx.fillStyle = pattern;
+          ctx.save();
+          ctx.translate(objects[k].pos.x - cameraOffset.x, objects[k].pos.y - cameraOffset.y);
+          // Scale to fit entire screen and then scale down to right size
+          var s = c.height / textures[objects[k].texture].height;
+          var repeatSize = objects[k].repeatSize;
+          ctx.scale(s, s);
+          ctx.scale(repeatSize, repeatSize);
+          ctx.fillRect(0, 0, objects[k].size.x / s / repeatSize, objects[k].size.y / s / repeatSize);
+          ctx.restore();
+        } else {
+          ctx.drawImage(textures[objects[k].texture], objects[k].pos.x - cameraOffset.x, objects[k].pos.y - cameraOffset.y, objects[k].size.x, objects[k].size.y);
+        }
+      }
+    }
+  }
+  
+  if(cheatMode) {
+    ctx.fillStyle = "rgba(255, 0, 0, 0.5)";
+    ctx.fillRect(player.pos.x - cameraOffset.x, player.pos.y - cameraOffset.y, player.size.x, player.size.y);
+  }
+  var w = textures[player.texture].width / (textures[player.texture].height / player.size.y);
+  
+  if(player.textureFlipped) {
+    ctx.save();
+    ctx.scale(-1, 1);
+    ctx.drawImage(textures[player.texture], 
+                -(player.pos.x - cameraOffset.x - (-w - player.size.x) / 2), 
+                player.pos.y - cameraOffset.y, 
+                w, 
+                player.size.y);
+    ctx.restore();
+  } else {
+    ctx.drawImage(textures[player.texture], 
+                player.pos.x - cameraOffset.x - (w - player.size.x) / 2, 
+                player.pos.y - cameraOffset.y, 
+                w, 
+                player.size.y);
+  }
+  ctx.globalCompositeOperation = "source-over";
+  
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fillRect(0, 0, 140, 80);
+  
+  ctx.fillStyle = "#000000";
+  ctx.font = "25px Arial";
+  ctx.fillText("fps: " + Math.round(actualFps * 10) / 10, 10, 30);
+  ctx.fillText("ms: " + Math.round(time - prevTime), 10, 60);
+}
+
+
+function collideObjects(objects, onCollide) {
+  if(!cheatMode) {
+    for(var i = 0; i < objects.length; i++) {
+      var object = objects[i];
+
+      // Check collision in x direction
+      var player_ = JSON.parse(JSON.stringify(player));
+      player_.pos.x += player_.vel.x;
+      if(checkCollision(player_, object)) {
+        // Get closest side
+        if(Math.abs((object.pos.x + object.size.x) - player.pos.x) < 
+          Math.abs(object.pos.x - (player.pos.x + player.size.x))) {
+          side = 3;
+          eval(onCollide);
+        } else {
+          side = 1;
+          eval(onCollide);
+        }
+      }
+
+      // Check collision in y direction
+      player_ = JSON.parse(JSON.stringify(player));
+      player_.pos.y += player_.vel.y;
+      if(checkCollision(player_, object)) {
+        // Get closest side
+        if(Math.abs((object.pos.y + object.size.y) - player.pos.y) < 
+          Math.abs(object.pos.y - (player.pos.y + player.size.y))) {
+          side = 0;
+          eval(onCollide);
+        } else {
+          side = 2;
+          eval(onCollide);
+        }
+      }
+    }
+  }
+}
+
+// Solid collision
+function collide(side, object) {
+  switch(side) {
+  case 0: // Up
+    player.vel.y = 0;
+    player.pos.y = object.pos.y + object.size.y;
+    break;
+  case 1: // Right
+    player.vel.x = 0;
+    player.pos.x = object.pos.x - player.size.x;
+    player.onWall = true;
+    break;
+  case 2: // Down
+    player.vel.y = 0;
+    player.pos.y = object.pos.y - player.size.y;
+    player.onGround = true;
+    player.wallJumped = false;
+    break;
+  case 3: // Left
+    player.vel.x = 0;
+    player.pos.x = object.pos.x + object.size.x;
+    player.onWall = true;
+    break;
+  }
+}
+
+// AABB
+function checkCollision(colliderA, colliderB) {
+  if(colliderA.pos.x + colliderA.size.x > colliderB.pos.x &&
+    colliderA.pos.y + colliderA.size.y > colliderB.pos.y &&
+    colliderA.pos.x < colliderB.pos.x + colliderB.size.x && 
+    colliderA.pos.y < colliderB.pos.y + colliderB.size.y) {
+    return true;  
+  } else {
+    return false;
+  }
+}
+
+window.onmousedown = function(e) {
+  if(cheatMode) {
+    if(!building) {
+      levels[0].walls.push(new wall(Math.round(e.clientX + cameraOffset.x), Math.round(e.clientY + cameraOffset.y), 0.1 * c.height, 0.1 * c.height, 1));
+      building = true;
+      return;
+    }
+    if(building) {
+      building = false;
+      var wall_ = levels[0].walls[levels[0].walls.length - 1];
+      console.log("data[0].walls.push(new wall(" + 
+                  wall_.pos.x / c.height + ", " + 
+                  wall_.pos.y / c.height + ", " + 
+                  wall_.size.x / c.height + ", " + 
+                  wall_.size.y / c.height + ", 1));");
+      return;
+    }
+  }
+}
+
+window.onmousemove = function(e) {
+  if(cheatMode && building) {
+    var wall_ = levels[0].walls[levels[0].walls.length - 1];
+    wall_.size.x = e.clientX + cameraOffset.x - wall_.pos.x;
+    wall_.size.y = e.clientY + cameraOffset.y - wall_.pos.y;
+  }
+}
+
+window.onkeydown = function(e) {
+  switch(e.keyCode) {
+  case 39: // Right
+  case 68:
+    player.acc.x = player.speed;
+    break;
+    
+  case 37: // Left
+  case 65:
+    player.acc.x = -player.speed;
+    break;
+    
+  case 38: // Up
+  case 87:
+    if(!cheatMode) {
+      if(player.onGround) {
+        player.vel.y = -player.jumpStrength;
+        player.onGround = false;
+      } else if(player.onWall && !player.wallJumped) {
+        player.vel.y = -player.jumpStrength;
+        player.vel.x += player.wallJumpStrength * -Math.max(Math.min(player.acc.x, 1), -1);
+        player.onWall = false;
+        player.wallJumped = true;
+      }
+    } else {
+      player.acc.y = -player.speed;
+    }
+    break;
+    
+  case 83:
+  case 40:
+    if(cheatMode) {
+      player.acc.y = player.speed
+    }
+    break;
+    
+  case 8:
+    cheatMode = !cheatMode;
+    player.vel.x = 0;
+    player.vel.y = 0;
+    player.acc.x = 0;
+    player.acc.y = 0;
+    break;
+  
+  case 32:
+    player.interacting = true;
+    break;
+  }
+}
+
+window.onkeyup = function(e) {
+  switch(e.keyCode) {
+  case 39: // Right
+  case 68:
+    if(player.acc.x > 0) {
+      player.acc.x = 0;
+    }
+    if(cheatMode) {
+      player.vel.x = 0;
+    }
+    break;
+    
+  case 37: // Left
+  case 65:
+    if(player.acc.x < 0) {
+      player.acc.x = 0;
+    }
+    if(cheatMode) {
+      player.vel.x = 0;
+    }
+    break;
+    
+  case 38: // Up
+  case 87:
+    if(cheatMode) {
+      player.acc.y = 0;
+      player.vel.y = 0;
+    }
+    break;
+    
+  case 83:
+  case 40:
+    if(cheatMode) {
+      player.acc.y = 0
+      player.vel.y = 0;
+    }
+    break;
+    
+  case 32:
+    player.interacting = false;
+    break;
+  }
+}
