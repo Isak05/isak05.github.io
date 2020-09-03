df57bf76033fb83a0247004a12f6174a6d8896d9
Isak05 <51046377+Isak05@users.noreply.github.com>
2020-09-03 07:40:58 +0200

    Add files via upload

--- Big Oof/main.js
+++ Big Oof/main.js
@@ -3,6 +3,7 @@ c.width = window.innerWidth;
 c.height = window.innerHeight;
 document.body.appendChild(c);
 var ctx = c.getContext("2d");
+var cData = ctx.getImageData(0, 0, c.width, c.height);
 
 var file = document.getElementById("file");
 var text = document.getElementById("text");
@@ -13,10 +14,17 @@ var conCtx = con.getContext("2d");
 var consoleText = "";
 var fr = new FileReader();
 
+var keys = [];
+for(var i = 0; i < 256; i++) keys.push(0);
+
 var view = 0;
 var lines = [];
 var line = 0;
 var variables = [];
+var functions = [];
+var stack = [];
+
+reset();
 
 function update() {
   if(view == 1) {
@@ -25,13 +33,13 @@ function update() {
       var curLine = lines[line];
       var found = false;
       
-      var l = curLine.match(/\/\/.*/);
+      var l = curLine.match(/^\s*\/\/.*/);
       if(l == null) {
         
-        l = curLine.match(/def +var\((.*)\) *= *([a-zA-Z]+) *=.*/);
+        l = curLine.match(/^\s*def +var\((.*)\) *= *([a-zA-Z]+) *=.*/);
         if(l != null && l.length == 3) {
           found = true;
-          if(l[1] != "int" && l[1] != "float") {
+          if(l[1] != "int" && l[1] != "float" && l[1] != "bool") {
             console.error('Invalid type "' + l[1] + '" on line ' + line);
           } else {
             var val = curLine.match(/def +var\(.*\) *= *[a-zA-Z]+ *= *(.+)$/);
@@ -43,8 +51,27 @@ function update() {
             }
           }
         }
+        
+        l = curLine.match(/^\s*def +fvar\((.*) *\, *(.*)\) *= *([a-zA-Z]+) *=.*/);
+        if(l != null && l.length == 4) {
+          found = true;
+          if(l[2] != "int" && l[2] != "float" && l[2] != "bool") {
+            console.error('Invalid type "' + l[2] + '" on line ' + line);
+          } else {
+            var val = curLine.match(/def +fvar\(.* *\, *.*\) *= *[a-zA-Z]+ *= *(.+)$/);
+            if(val == null) {
+              console.error('Invalid variable declaration on line ' + line);
+            } else {
+              val = evalExp(val[1]);
+              if(functions[l[1]] == undefined) {
+                functions[l[1]] = {line: undefined, vars: []};
+              }
+              functions[l[1]].vars[l[3]] = new variable(val, l[2]);
+            }
+          }
+        }
 
-        l = curLine.match(/exe +(if|while)\((.*)\):$/);
+        l = curLine.match(/^\s*exe +(if|while)\((.*)\):$/);
         if(l != null && l.length == 3) {
           found = true;
           var res = evalExp(l[2]);
@@ -52,10 +79,10 @@ function update() {
             var tempLine = line;
             var scope = 0;
             while(true) {
-              if(lines[tempLine].match(/exe +(if|while)\((.*)\):$/) != null) {
+              if(lines[tempLine].match(/^\s*exe +(if|while)\((.*)\):$/) != null) {
                 scope++;
               }
-              if(lines[tempLine].match(/exe +end;$/) != null) {
+              if(lines[tempLine].match(/^\s*exe +end;$/) != null) {
                 scope--;
               }
 
@@ -72,41 +99,166 @@ function update() {
           }
         }
         
-        l = curLine.match(/exe end;$/);
+        l = curLine.match(/^\s*exe end;$/);
         if(l != null && l.length == 1) {
           found = true;
           var start = line;
           var tempLine = line;
           var scope = 0;
           while(true) {
-            if(lines[tempLine].match(/exe +(if|while)\((.*)\):$/) != null) {
+            if(lines[tempLine].match(/^\s*exe +(if|while)\((.*)\):$/) != null || lines[tempLine].match(/^\s*def +(func)\((.*)\):$/)) {
               scope--;
             }
-            if(lines[tempLine].match(/exe +end;$/) != null) {
+            if(lines[tempLine].match(/^\s*exe +end;$/) != null) {
               scope++;
             }
 
             if(scope == 0) {
-              var temp = lines[tempLine].match(/exe +while\((.*)\):$/);
+              var temp = lines[tempLine].match(/^\s*exe +while\((.*)\):$/);
               if(temp != null && temp.length == 2) {
                 line = tempLine - 1;
                 break;
               } else {
-                break;
+                temp = lines[tempLine].match(/^\s*def +func\((.*)\):$/);
+                if(temp != null && temp.length == 2) {
+                  line = stack.pop().line;
+                  break;
+                } else {
+                  break;
+                }
               }
             }
 
             tempLine--;
-            if(tempLine <= 0) {
+            if(tempLine < 0) {
               break;
             }
           }
         }
         
-        l = curLine.match(/exe print\((.*)\)$/);
-        if(l != null && l.length == 2) {
+        l = curLine.match(/^\s*([^ ]+?) *([+\-*/=]{1,2}) *(.*)$/);
+        if(l != null && l.length >= 3) {
+          found = true;
+          var v = 0;
+          var a = 0;
+          var b = 0;
+          var f = false;
+          
+          var temp = curLine.match(/^\s*get\.var\((.+?)\) *[+\-*/=]{1,2}/);
+          if(temp != null && temp.length == 2) {
+            f = true;
+            if(stack.length > 0) {
+              a = functions[stack[stack.length - 1].name].vars[temp[1]].value;
+              b = new variable(evalExp(l[3]), functions[stack[stack.length - 1].name].vars[temp[1]].type).value;
+            }
+          } else {
+            a = variables[l[1]].value;
+            b = new variable(evalExp(l[3]), variables[l[1]].type).value;
+          }
+          switch(l[2]) {
+          case "+=":
+            if(l.length == 4) {
+              v = a + b;
+            } else {
+              console.error("No value to be added at line " + line);
+            }
+            break;
+          case "-=":
+            if(l.length == 4) {
+              v = a - b;
+            } else {
+              console.error("No value to be subtracted at line " + line);
+            }
+            break;
+          case "*=":
+            if(l.length == 4) {
+              v = a * b;
+            } else {
+              console.error("No value to be multiplied at line " + line);
+            }
+            break;
+          case "/=":
+            if(l.length == 4) {
+              v = a / b;
+            } else {
+              console.error("No value to be divided at line " + line);
+            }
+            break;
+          case "++":
+            v = a + 1;
+            break;
+          case "--":
+            v = a - 1;
+            break;
+          case "=":
+            v = b;
+            break;
+          }
+          
+          if(f) {
+            functions[stack[stack.length - 1].name].vars[temp[1]].setValue(v);
+          } else {
+            variables[l[1]].setValue(v);
+          }
+        }
+        
+        l = curLine.match(/^\s*exe +print\((.*)\)$/);
+        if(!found && l != null && l.length == 2) {
           found = true;
           console.log(evalExp(l[1]));
+        }
+        
+        l = curLine.match(/^\s*exe +func\(([a-zA-Z]+)\)$/);
+        if(!found && l != null) {
+          found = true;
+          if(l[1] == "getKey") {
+            functions[l[1]].vars["state"].setValue(keys[functions[l[1]].vars["key"].value]);
+          } else if(l[1] == "drawPixel") {
+            var index = functions[l[1]].vars["x"].value + functions[l[1]].vars["y"].value * cData.width;
+            cData.data[index * 4 + 0] = functions[l[1]].vars["r"].value;
+            cData.data[index * 4 + 1] = functions[l[1]].vars["g"].value;
+            cData.data[index * 4 + 2] = functions[l[1]].vars["b"].value;
+            cData.data[index * 4 + 3] = 255;
+          } else {
+            if(functions[l[1]] == undefined) {
+              console.error('Undefined function "' + l[1] + '"');
+            } else {
+              stack.push({line, name: l[1]});
+              line = functions[l[1]].line;
+            }
+          }
+        }
+        
+        l = curLine.match(/^\s*def +func\((.*)\):$/);
+        if(!found && l != null && l.length == 2) {
+          found = true;
+          if(functions[l[1]] == undefined)
+            functions[l[1]] = {line: line, vars: []};
+          if(functions[l[1]].line == undefined)
+            functions[l[1]].line = line;
+          if(functions[l[1]].vars == undefined)
+            functions[l[1]].vars = [];
+          
+          var tempLine = line;
+          var scope = 0;
+          while(true) {
+            if(lines[tempLine].match(/^\s*exe +(if|while)\((.*)\):$/) != null || lines[tempLine].match(/^\s*def +(func)\((.*)\):$/) != null) {
+              scope++;
+            }
+            if(lines[tempLine].match(/^\s*exe +end;$/) != null) {
+              scope--;
+            }
+
+            if(scope == 0) {
+              line = tempLine;
+              break;
+            }
+
+            tempLine++;
+            if(tempLine >= lines.length) {
+              break;
+            }
+          }
         }
         
         if(!found && curLine.search(/^\s*$/) == -1) {
@@ -122,8 +274,13 @@ function update() {
       }
     }
     
+    draw();
     requestAnimationFrame(update);
   }
+}
+
+function draw() {
+  ctx.putImageData(cData, 0, 0);
 }
 
 drawConsole();
@@ -209,8 +366,10 @@ function calc(val1, val2, oper) {
 }
 
 function evalExp(exp) {
+  exp += "";
+  exp.replace(/\s/g, "");
   for(var i = 0; i < exp.length; i++) {
-    if(exp[i].search(/^[a-zA-Z"']$/) != -1) {
+    if(exp[i].search(/^[a-zA-Z"'\.]$/) != -1 && (i == 0 || exp[i - 1].search(/[0-9]/) == -1)) {
       var str = exp[i];
       var quote = "";
       if(str == '"' || str == "'") {
@@ -226,7 +385,7 @@ function evalExp(exp) {
           break;
         }
         if(quote == "") {
-          if(exp[i].search(/^[a-zA-Z]$/) != -1) {
+          if((exp[i].search(/^[a-zA-Z\.\, ]$/) != -1 || (exp[i - 1].search(/[\(]/) != -1 && exp[i].search(/[\)]/) != -1)) && exp[i - 1].search(/[0-9]/) == -1) {
             str += exp[i];
           } else {
             i--;
@@ -250,15 +409,32 @@ function evalExp(exp) {
           rep = 0;
           break;
         default:
-          rep = variables[str].value;
+          if(str.substr(0, 8) == "get.var(") {
+            rep = functions[stack[stack.length - 1].name].vars[str.substr(8, str.length - 9)].value;
+          } else if(str.substr(0, 9) == "get.fvar(") {
+            rep = functions[str.substr(9, str.length - 10).match(/(.*)\,/)[1]].vars[str.substr(9, str.length - 10).match(/\, *(.*)/)[1]].value;
+          } else if(str != ".") {
+            rep = variables[str.replace(/[^a-zA-Z]/g, "")].value;
+          } else {
+            rep = ".";
+          }
           break;
         }
+        
         exp = exp.substr(0, start) + rep + exp.substr(start + len);
         i -= len;
       }
     }
   }
   
+  s = 0;
+  while(s != -1 && s != null) {
+    s = exp.match(/(?<=[+\-*/<>=]|^) *(\- *[0-9]+\.{0,1}[0-9]*)/);
+    if(s != null && s.length == 2) {
+      exp = exp.substr(0, s.index) + "(0" + s[1] + ")" + exp.substr(s.index + s[0].length);
+    }
+  }
+
   var values = [];
   var operators = [];
   for(var i = 0; i < exp.length; i++) {
@@ -350,14 +526,29 @@ function variable(value, type) {
     switch(this.type) {
     case "int":
       this.value = parseInt(value);
+      if(value == "" || value == undefined) {
+        this.value = 0;
+      }
       if(isNaN(this.value)) {
         console.error('Couldn\'t parse int "' + value + '"');
       }
       break;
     case "float":
       this.value = parseFloat(value);
+      if(value == "" || value == undefined) {
+        this.value = 0;
+      }
       if(isNaN(this.value)) {
         console.error('Couldn\'t parse float "' + value + '"');
+      }
+      break;
+    case "bool":
+      this.value = Math.max(0, Math.min(1, value));
+      if(value == "" || value == undefined) {
+        this.value = 0;
+      }
+      if(isNaN(this.value)) {
+        console.error('Couldn\'t parse bool "' + value + '"');
       }
       break;
     }
@@ -379,7 +570,23 @@ function run() {
 
 function reset() {
   variables = [];
+  functions = [];
+  stack = [];
   consoleText = "";
+  var vars = [];
+  vars["key"] = new variable(0, "int");
+  vars["state"] = new variable(0, "bool");
+  functions["getKey"] = {line: -1, vars};
+  vars = [];
+  vars["x"] = new variable(0, "int");
+  vars["y"] = new variable(0, "int");
+  vars["r"] = new variable(0, "int");
+  vars["g"] = new variable(0, "int");
+  vars["b"] = new variable(0, "int");
+  functions["drawPixel"] = {line: -1, vars};
+  
+  ctx.clearRect(0, 0, c.width, c.height);
+  cData = ctx.getImageData(0, 0, c.width, c.height);
 }
 
 function stop() {
@@ -420,9 +627,15 @@ window.onkeydown = function(e) {
       run();
     }
   }
+  keys[e.keyCode] = true;
+}
+
+window.onkeyup = function(e) {
+  keys[e.keyCode] = false;
 }
 
 window.onresize = function() {
   c.width = window.innerWidth;
   c.height = window.innerHeight;
+  cData = ctx.getImageData(0, 0, c.width, c.height);
 }
