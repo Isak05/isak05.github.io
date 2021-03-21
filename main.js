bba0a307a60241591e26c93d5017e04e5694ff78
Isak Wecksten <isak.wecksten@gmail.com>
2021-03-21 20:25:15 +0100

    Continued on homepage

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-console.log("Website being reworked");
+window.addEventListener("scroll", function(e) {
+	document.documentElement.style.setProperty("--scroll-position", window.scrollY);
+	console.log("A");
+});
